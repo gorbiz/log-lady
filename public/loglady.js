7e6f8c986cb8cb27466ac0cd5a513465d52a7d0b
Karl Svartholm <gorbiz@gmail.com>
2023-12-16 13:29:21 +0100

    cleanup loglady.js WIP

--- public/loglady.js
+++ public/loglady.js
@@ -3,31 +3,29 @@
 // storage "engine" (can be replaced with IndexedDB, etc.)
 function Storage (name) {
   const storage = {
-    get (id = null, excludeDeleted = true) {
+    get ({ id = null, includeDeleted = false } = {}) {
       const logs = JSON.parse(localStorage.getItem(name)) || {}
-      if (id) return logs[id]
-
-      return Object.values(logs).filter(log => !(excludeDeleted && log.deleted))
+      if (id) return logs[id] // NOTE ignores includeDeleted here
+      return Object.values(logs).filter(log => (includeDeleted || log.deleted))
     },
-    set (entriesObj) {
+    set (entriesObj) { // NOTE set all
       localStorage.setItem(name, JSON.stringify(entriesObj))
     },
-    add (entry) {
+    add (entry) { // TODO rename to upsert?
       const entries = JSON.parse(localStorage.getItem(name)) || {}
       entries[entry.id] = entry
       this.set(entries)
     },
-    update (entry) {
+    update (entry) { // TODO upsert instead, see above?
       const entries = JSON.parse(localStorage.getItem(name)) || {}
       entries[entry.id] = { ...entries[entry.id], ...entry }
       this.set(entries)
     },
-    remove (id) {
+    remove (id) { // almost never used (soft delete instead)
       const entries = JSON.parse(localStorage.getItem(name)) || {}
-      if (entries[id]) {
-        delete entries[id]
-        this.set(entries)
-      }
+      if (!entries[id]) return
+      delete entries[id]
+      this.set(entries)
     },
     clear () {
       this.set({})
@@ -36,10 +34,51 @@ function Storage (name) {
   return storage
 }
 
+function Http (baseUrl, storage) { // NOTE passing storage is a bit convoluted... but handy also?
+  function markSynced (entry) {
+    delete entry.synced
+    storage.update(entry)
+  }
+
+  return {
+    get ({ id = '', since } = {}) {
+      let url = `${baseUrl}/${id}`
+      if (since) url += `?since=${since}`
+      return fetch(url)
+    },
+    post (entry) {
+      if (!navigator.onLine) return false
+      return fetch(baseUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(entry)
+      })
+      .then(response => {
+        if (response.ok) markSynced(entry)
+        return response
+      })
+    },
+    put (entry) {
+      if (!navigator.onLine) return false
+      return fetch(`${baseUrl}/${entry.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(entry)
+      })
+      .then(response => {
+        if (response.ok) markSynced(entry)
+        return response
+      })
+    },
+    // delete (id) { ... }
+  }
+}
+
 function LogLady ({ storage, server, bucket = 'logs' } = { }) {
   storage = storage || Storage('loglady')
   if (server === true) server = '/api'
   const apiUrl = server ? `${server}/${bucket}` : null
+  let http = server ? Http(apiUrl, storage) : null
 
   // Call fetchUpdates when the app starts or comes online
   if (server) fetchUpdates()
@@ -49,45 +88,34 @@ function LogLady ({ storage, server, bucket = 'logs' } = { }) {
     window.dispatchEvent(new CustomEvent(eventName, { detail }))
   }
 
-  async function syncLogWithServer (log, method = 'POST') {
-    const endpoint = method === 'DELETE' ? `${apiUrl}/${log.id}` : apiUrl
-    const body = method === 'DELETE' ? null : JSON.stringify(log)
-    await fetch(endpoint, {
-      method,
-      headers: { 'Content-Type': 'application/json' },
-      body
+  
+  const lastSync = localStorage.getItem('lastSync') || '1970-01-01T00:00:00.000Z'
+  function sync () {
+    const entries = http.get({ since: lastSync }) // TODO api.get
+    entries.forEach(log => {
+      const current = storage.get(log.id)
+      if (current.modified >= log.modified) return
+      storage.set(log)
+      // emit('logAdded', log) // TODO or? emit('logUpdated', update)
     })
-    delete log.synced
-    storage.update(log)
+    localStorage.setItem('lastSync', new Date().toISOString()) // TODO get from server instead
+    if (updates.length) emit('logsChanged') // basic 1.0
+    // later trigger one event per entry, oldest modified first
   }
-
-  async function fetchUpdates () {
-    const lastSyncDate = localStorage.getItem('lastSyncDate') || '1970-01-01T00:00:00.000Z'
-    try {
-      const response = await fetch(`${apiUrl}?lastSyncDate=${lastSyncDate}`)
-      if (!response.ok) throw new Error('Network response was not ok')
-
-      const updates = await response.json()
-
-      // Process updates: add new logs, update existing, and handle deletions
-      updates.forEach(update => {
-        if (update.deleted) {
-          storage.remove(update.id)
-        } else {
-          storage.update(update)
-        }
-      })
-
-      // Update lastSyncDate in localStorage
-      localStorage.setItem('lastSyncDate', new Date().toISOString())
-    } catch (error) {
-      console.error('Fetch updates failed:', error)
-    }
-  }
+    
+  // TODO realtime sync, pseudo-code:
+  // io.on('added / modified', (entry) => {
+  //   return sync()
+  //   // Maybe 2.0
+  //   current = storage.get(entry)
+  //   if current.modified >= entry.modified return
+  //   storage.upsert(entry)
+  //   emit logsChanged
+  // }    
 
   return {
     get (id = null) {
-      return storage.get(id)
+      return storage.get({ id })
     },
     async add ({ text, created, id, extra } = {}) { // normally only text is passed
       if (!text) return false
@@ -100,18 +128,17 @@ function LogLady ({ storage, server, bucket = 'logs' } = { }) {
       storage.add(log)
       emit('logAdded', log)
 
-      if (server && navigator.onLine) await syncLogWithServer(log)
+      if (server) http.post(log)
     },
     async remove (id) {
-      let log = storage.get(id)
+      let log = storage.get({ id })
       if (!log) return
-      const now = new Date()
-      log = { ...log, deleted: now, modified: now, synced: false }
+      log = { ...log, deleted: true, modified: new Date(), synced: false }
 
       storage.update(log) // used to be: storage.remove(id)
       emit('logRemoved', { id })
 
-      if (server && navigator.onLine) await syncLogWithServer(log, 'DELETE')
+      if (server) http.put(log)
     },
     fetchUpdates,
     clear () {
